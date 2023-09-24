73b9cee1dcb61a547df3fe8e45ef27029a879440
kitoga <jken04680@gmail.com>
2023-09-24 06:03:42 +0100

    things

--- lib/src/repositories/explore_repository.dart
+++ lib/src/repositories/explore_repository.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:chases_scroll/src/models/community_model.dart';
 import 'package:chases_scroll/src/repositories/api/api_clients.dart';
 import 'package:chases_scroll/src/repositories/endpoints.dart';
@@ -14,43 +12,31 @@ final repositoryProvider = Provider<ExploreRepository>(
 
 class ExploreRepository {
   List<ContentEvent> eventList = [];
-  Future<List<ContentEvent>> getTopEvents() async {
-    final response =
-        await ApiClient.get(Endpoints.getTopEvents, useToken: true);
-
-    if (response.status == 200) {
-      final List<dynamic> topEvents = response.message['content'];
-      //log(topEvents.toString());
-      eventList = topEvents
-          .map<ContentEvent>((event) => ContentEvent.fromJson(event))
-          .toList();
-
-      return eventList;
-    } else {
-      return [];
-    }
-  }
-
   //this is for getting suggested users
   List<Content> suggestedUsers = [];
-  Future<List<Content>> getSuggestedUsers() async {
-    final response =
-        await ApiClient.get(Endpoints.getSuggestedUsers, useToken: true);
-
-    if (response.status == 200 || response.status == 201) {
-      final List<dynamic> suggestUsers = response.message['content'];
-      //log(suggestUsers.toString());
-      suggestedUsers =
-          suggestUsers.map<Content>((user) => Content.fromJson(user)).toList();
-
-      return suggestedUsers;
-    } else {
-      return [];
-    }
-  }
 
   //this is for all event
   List<ContentEvent> allEventList = [];
+  //this is for community
+  List<CommContent> allCommunityList = [];
+
+  Future<List<CommContent>> getAllCommunity() async {
+    final response =
+        await ApiClient.get(Endpoints.getAllCommunities, useToken: true);
+
+    if (response.status == 200) {
+      final List<dynamic> allCommunities = response.message['content'];
+      //log(allCommunities.toString());
+      allCommunityList = allCommunities
+          .map<CommContent>((event) => CommContent.fromJson(event))
+          .toList();
+
+      return allCommunityList;
+    } else {
+      return [];
+    }
+  }
+
   Future<List<ContentEvent>> getAllEvents() async {
     final response =
         await ApiClient.get(Endpoints.getAllEvents, useToken: true);
@@ -68,20 +54,34 @@ class ExploreRepository {
     }
   }
 
-  //this is for community
-  List<CommContent> allCommunityList = [];
-  Future<List<CommContent>> getAllCommunity() async {
+  Future<List<Content>> getSuggestedUsers() async {
     final response =
-        await ApiClient.get(Endpoints.getAllCommunities, useToken: true);
+        await ApiClient.get(Endpoints.getSuggestedUsers, useToken: true);
+
+    if (response.status == 200 || response.status == 201) {
+      final List<dynamic> suggestUsers = response.message['content'];
+      //log(suggestUsers.toString());
+      suggestedUsers =
+          suggestUsers.map<Content>((user) => Content.fromJson(user)).toList();
+
+      return suggestedUsers;
+    } else {
+      return [];
+    }
+  }
+
+  Future<List<ContentEvent>> getTopEvents() async {
+    final response =
+        await ApiClient.get(Endpoints.getTopEvents, useToken: true);
 
     if (response.status == 200) {
-      final List<dynamic> allCommunities = response.message['content'];
-      //log(allCommunities.toString());
-      allCommunityList = allCommunities
-          .map<CommContent>((event) => CommContent.fromJson(event))
+      final List<dynamic> topEvents = response.message['content'];
+      //log(topEvents.toString());
+      eventList = topEvents
+          .map<ContentEvent>((event) => ContentEvent.fromJson(event))
           .toList();
 
-      return allCommunityList;
+      return eventList;
     } else {
       return [];
     }
